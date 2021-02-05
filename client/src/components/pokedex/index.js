5efab4574b431c0c5f0cd39e6f85df68450d3f7d
Reg <reggiegunawan88@gmail.com>
2021-02-05 20:59:56 +0700

    Pagination - Load More

--- client/src/components/pokedex/index.js
+++ client/src/components/pokedex/index.js
@@ -3,20 +3,26 @@ import PokemonCard from "./pokemon-card/card";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_POKEMONS } from "./../utils/get-pokemons";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import "./index.css";
 
 const index = () => {
   const { data } = useQuery(GET_POKEMONS, {
-    variables: { first: 200 },
+    variables: { first: 500 },
   });
 
   const [ownedPokemons, setOwnedPokemons] = useState(0);
+  const [limitPage, setLimitPage] = useState(6);
 
   useEffect(() => {
     if (localStorage.getItem("myPokemon")) {
       setOwnedPokemons(JSON.parse(localStorage.getItem("myPokemon")).length);
     }
   }, []);
+
+  const loadMore = () => {
+    setLimitPage(limitPage + 6);
+  };
 
   if (data === undefined) {
     return <h3>Loading...</h3>;
@@ -30,12 +36,24 @@ const index = () => {
         </div>
         <div style={{ margin: 50 }}>
           <Grid container spacing={4}>
-            {data.pokemons.map((pokemon) => (
-              <Grid item xs={4} key={pokemon.number}>
+            {data.pokemons.slice(0, limitPage).map((pokemon) => (
+              <Grid className="fade-in" item xs={4} key={pokemon.number}>
                 <PokemonCard pokemon={pokemon} />
               </Grid>
             ))}
           </Grid>
+          {limitPage < data.pokemons.length && (
+            <div container>
+              <Button
+                style={{ margin: 20, borderRadius: 5 }}
+                variant="contained"
+                color="primary"
+                onClick={loadMore}
+              >
+                Load More...
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
